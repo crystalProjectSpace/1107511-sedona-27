c0e393daa61d7bf1dd467f76e84c6e38924659d3
AlishaRich <60033076+AlishaRich@users.noreply.github.com>
2020-03-15 20:31:58 +0300

    Личный проект: оживляем интерактивные элементы

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,32 @@
 var link = document.querySelector(".preview");
-var form = document.querySelector(".search-hotels")
+var form = document.querySelector(".search-hotels");
+var button = document.querySelector(".search");
+var input = document.querySelector("input[text]");
+
+
+document.addEventListener("DOMContentLoaded", function(evt) {
+  form.classList.add("visually-hidden");
+});
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
+  form.classList.toggle("visually-hidden");
   form.classList.remove("search-hotels-bounce");
   form.offsetWidth = form.offsetWidth;
   form.classList.add("search-hotels-bounce");
-  form.classList.toggle("visually-hidden");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (form.classList.contains("search-hotels")) {
+      form.classList.add("visually-hidden");
+    }
+  }
+});
+
+form.addEventListener("submit", function(evt){
+  if (!input.value) {
+    evt.preventDefault();
+  }
 });
