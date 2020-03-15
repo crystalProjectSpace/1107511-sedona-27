4bf3eb77916a97ab160eef23585fc5a38f28a1b2
AlishaRich <60033076+AlishaRich@users.noreply.github.com>
2020-03-15 23:46:13 +0300

    Личный проект: оживляем интерактивные элементы

--- js/popup.js
+++ js/popup.js
@@ -1,39 +1,35 @@
 var link = document.querySelector(".preview");
 var form = document.querySelector(".search-hotels");
-var button = document.querySelector(".search");
 var arrival = form.querySelector("[name=arrival-date]");
 var departure = form.querySelector("[name=departure-date]");
 var adult = form.querySelector("[name=adult-number]");
 var child = form.querySelector("[name=child-number]");
 
 document.addEventListener("DOMContentLoaded", function(evt) {
-  form.classList.add("visually-hidden");
+	form.classList.add("visually-hidden");
 });
-
 link.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  form.classList.toggle("visually-hidden");
-  form.classList.remove("search-hotels-bounce");
-  form.offsetWidth = form.offsetWidth;
-  form.classList.add("search-hotels-bounce");
-  arrival.focus();
+	evt.preventDefault();
+	form.classList.toggle("visually-hidden");
+	form.classList.remove("search-hotels-bounce");
+	form.offsetWidth = form.offsetWidth;
+	form.classList.add("search-hotels-bounce");
+	arrival.focus();
 });
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (form.classList.contains("search-hotels")) {
-      form.classList.add("visually-hidden");
-    }
-  }
+window.addEventListener("keydown", function(evt) {
+	if(evt.keyCode === 27) {
+		evt.preventDefault();
+		if(form.classList.contains("search-hotels")) {
+			form.classList.add("visually-hidden");
+		}
+	}
 });
-
-form.addEventListener("submit", function(evt){
-  if (!arrival.value || !departure.value || !adult.value ||  !child.value) {
-    evt.preventDefault();
-    alert("Только 2");
-    console.log("Нужно ввести данные");
-} else {
-    localStorage.setItem("search-hotels-number", adult.value);
-  }
+form.addEventListener("submit", function(evt) {
+	if(!arrival.value || !departure.value || !adult.value || !child.value) {
+		evt.preventDefault();
+		console.log("Нужно ввести данные");
+		form.classList.remove("search-hotels-error");
+		form.offsetWidth = form.offsetWidth;
+		form.classList.add("search-hotels-error");
+	}
 });
