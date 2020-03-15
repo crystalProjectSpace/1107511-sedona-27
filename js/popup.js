b9bff86e2c9afa94a20e7f14b6d325d3fe923289
AlishaRich <60033076+AlishaRich@users.noreply.github.com>
2020-03-15 12:52:23 +0300

    Личный проект: оживляем интерактивные элементы

--- js/popup.js
+++ js/popup.js
@@ -3,5 +3,8 @@ var form = document.querySelector(".search-hotels")
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
+  form.classList.remove("search-hotels-bounce");
+  form.offsetWidth = form.offsetWidth;
+  form.classList.add("search-hotels-bounce");
   form.classList.toggle("visually-hidden");
 });
